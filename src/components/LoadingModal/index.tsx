8c17108b8213b7145b9278bf0321c76c591d7eba
Deivison Cardoso <deivisondc@gmail.com>
2021-05-16 12:34:17 -0300

    add loading modal component

---
+++ src/components/LoadingModal/index.tsx
@@ -0,0 +1,17 @@
+import { VscLoading } from 'react-icons/vsc';
+
+import GlassModal from '../GlassModal';
+
+import styles from './styles.module.scss';
+
+interface LoadingModalProps {
+  isOpen: boolean;
+}
+
+export default function LoadingModal({ isOpen }: LoadingModalProps) {
+  return (
+    <GlassModal square isOpen={isOpen}>
+      <VscLoading className={styles.rotatingIcon} size={48} />
+    </GlassModal>
+  );
+}
